90380524a626b825a1dfce8f88ea3765387c2f35
Ryuya Watanabe <r.watanabe.sys21@morijyobi.ac.jp>
2022-03-10 10:22:46 +0900

    "こんにちは" "さようなら"

--- pra2.java
+++ pra2.java
@@ -1,1 +1,9 @@
-System.out.println("hello World");
+public class Main {
+    public static void main(String[] args) {
+	    int i;
+	    for(i = 0 , i < 100 , i + 1){
+		System.out.println("hello World");
+	    }
+    }
+}
+
